156e81ec55a970ddac11e3897d5629b16d091ca8
d-w-moore <dmoore@renci.org>
2023-08-01 18:35:42 -0400

    _list () can operate with simple session

--- tryticketsimple.py
+++ tryticketsimple.py
@@ -1,9 +1,11 @@
 from irods.ticket import Ticket
-#from irods.session import iRODSSession as Session
+from irods.session import iRODSSession, _list
 from irods.experimental.client.simple import Session
 from irods.models import DataObject,Collection
 
 s = Session(irods_env_file='/home/daniel/.irods/irods_environment.json')
+_list(s,s,session_class = type(s))
+#exit()
 Ticket(s,'myticket').supply()
 q = s.query(Collection,DataObject)
 l = list(q)
