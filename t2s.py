42b173cdfae6867b8fea13febebcd1a9c4ec575f
d-w-moore <dmoore@renci.org>
2023-08-03 16:05:41 -0400

    changes to simplify demo script for irods/irods#7250

--- t2s.py
+++ t2s.py
@@ -5,30 +5,29 @@ from irods.experimental.client.simple import Session
 from irods.models import DataObject,Collection
 from datetime import datetime as dt
 
-optList,args = getopt.getopt(sys.argv[1:],'Ivg:t:')
+# Usage:
+# issue7250.py [-v] [-g EXPLICIT_HOSTNAME ] -t TICKET_STRING PATH
+# -v : call _list on original and cloned session
+# -t : ticket string to use on both sessions
+# -g : within this demo, -g supplies an hostname for the purpose of circumventing
+#      data_objects.open()'s normal redirect.
+
+optList,args = getopt.getopt(sys.argv[1:],'vg:t:')
 opts = dict(optList)
-option_I_given = ('-I' in opts) # allow session cloning internal to open() call.
 
-# Without -I optionmake a pre-existing session to "compete" with the one involved in the data open
+CLIENT_CONFIG = os.path.expanduser('~/.irods/irods_environment.json')
 
-CLIENT_CONFIG = os.expanduser('~/.irods/irods_environment.json')
+sess = Session(irods_env_file = CLIENT_CONFIG)
+Ticket(sess,opts['-t']).supply()
 
-if not option_I_given:
-    s1 = Session(irods_env_file = CLIENT_CONFIG)
-    Ticket(s1,opts['-t']).supply()
-    s1.pool.get_connection()
-    if '-v' in opts:
-        _list(s1,s1,session_class = type(s1))
-
-s2 = Session(irods_env_file = CLIENT_CONFIG)
-Ticket(s2,opts['-t']).supply()
 if '-v' in opts:
-    _list(s2,s2,session_class = type(s2))
+    _list(sess)
 
-path = args[0] # TODO delete: was '/tempZone/home/rods/pwfile.txt'
+path = args[0]
 
-o = s2.data_objects.open(path,
-                        returned_values = None if ('-g' not in opts) else {'GET':opts['-g']},
-                        allow_redirect = option_I_given # False value will prevent internal cloning of session
-                        )
+#
+# Inside of  sess.data_objects.open, GET_HOST_FOR_GET will be called unless -g was used
+o = sess.data_objects.open(path, 'r',
+                           returned_values = None if '-g' not in opts
+                                                  else {'GET':opts['-g']})
 o.close()
