53aad32abb85d83c16ab2a8610b781fe7a05b2f5
d-w-moore <dmoore@renci.org>
2024-02-07 04:15:00 -0500

    now able to define the irods version to be installed.

--- irods/test/harness/build-docker.sh
+++ irods/test/harness/build-docker.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-#IRODS_VERSION="4.3.1-0~bionic"
+# IRODS_PACKAGE_VERSION if defined is like "4.3.1-0~bionic"
+# (but contains no '~' suffix for irods versions <= 4.2.10)
 
 BASE=$(basename "$0")
 DIR=$(realpath "$(dirname "$0")")
@@ -9,5 +10,6 @@ DOCKER=docker
 for dockerfile in [0-9]*.Dockerfile; do 
     image_name=${dockerfile#[0-9]*_}
     image_name=${image_name%.Dockerfile}
-    $DOCKER build -f $dockerfile -t $image_name . ${NO_CACHE+"--no-cache"} || exit
+    $DOCKER build -f $dockerfile -t $image_name . ${IRODS_PACKAGE_VERSION:+"--build-arg=irods_package_version=$IRODS_PACKAGE_VERSION"} \
+                                                  ${NO_CACHE+"--no-cache"} || exit
 done
