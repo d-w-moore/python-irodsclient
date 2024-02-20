fd5b171eb60fdfd45d939fe08206f80ba065f26b
d-w-moore <dmoore@renci.org>
2024-02-20 08:01:49 -0500

    fixes

--- docker-testing/run_tests.sh
+++ docker-testing/run_tests.sh
@@ -5,5 +5,10 @@ cd "$DIR"
 ./recv_oneshot -h irods-provider -p 8888 -t 360
 REPO="$(realpath ./repo)"
 python -m pip install "$REPO[tests]"
-./iinit.py host irods-provider port 1247 user rods password rods zone tempZone 
-python $REPO/irods/test/runner.py
+./iinit.py \
+    host irods-provider \
+    port 1247 \
+    user rods \
+    password rods \
+    zone tempZone
+python "$REPO"/irods/test/runner.py
--- docker-testing/start_containers.sh
+++ docker-testing/start_containers.sh
@@ -2,5 +2,5 @@
 DIR=$(dirname "$0")
 cd "${DIR}"
 echo "python_version=${1}" >.env
-echo "repo_external=$(realpath "${DIR}/repo")" >>.env
-docker-compose -f harness-docker-compose.yml up -d
+echo "repo_external=$(realpath ./repo)" >>.env
+docker-compose -f harness-docker-compose.yml up -d --build
