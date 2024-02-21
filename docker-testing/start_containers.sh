25ad9b41762247e09e3fd4612f6612ef1479683c
d-w-moore <dmoore@renci.org>
2024-02-21 01:28:28 -0500

    in docker-compose, perform separate 'build' and 'up' separately

--- docker-testing/start_containers.sh
+++ docker-testing/start_containers.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+set -e
 DIR=$(dirname "$0")
 cd "${DIR}"
 echo "python_version=${1}" >.env
 echo "repo_external=$(realpath ./repo)" >>.env
-docker-compose -f harness-docker-compose.yml up -d --build
+docker-compose -f harness-docker-compose.yml build
+docker-compose -f harness-docker-compose.yml up -d
