af234909d248d315b1e941cedf394f55048ed6bc
d-w-moore <dmoore@renci.org>
2024-02-21 22:17:08 -0500

    correct providers name

--- docker-testing/run_tests.sh
+++ docker-testing/run_tests.sh
@@ -2,11 +2,11 @@
 set -e -x
 DIR=$(dirname "$0")
 cd "$DIR"
-./recv_oneshot -h irods-provider -p 8888 -t 360
+./recv_oneshot -h irods-catalog-provider -p 8888 -t 360
 REPO="$(realpath ./repo)"
 python -m pip install "$REPO[tests]"
 ./iinit.py \
-    host irods-provider \
+    host irods-catalog-provider \
     port 1247 \
     user rods \
     password rods \
