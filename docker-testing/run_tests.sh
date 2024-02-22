df02d6622429a7f7d383c4485f0eccd457de7a70
d-w-moore <dmoore@renci.org>
2024-02-21 21:45:39 -0500

    checkpoints - where are we failing

--- docker-testing/run_tests.sh
+++ docker-testing/run_tests.sh
@@ -5,10 +5,12 @@ cd "$DIR"
 ./recv_oneshot -h irods-provider -p 8888 -t 360
 REPO="$(realpath ./repo)"
 python -m pip install "$REPO[tests]"
+echo "pip install or PRC done"
 ./iinit.py \
     host irods-provider \
     port 1247 \
     user rods \
     password rods \
     zone tempZone
+echo "iinit done"
 python "$REPO"/irods/test/runner.py
