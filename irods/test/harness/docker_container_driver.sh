a07dcd8ddb7521facde67d5301f9dda49fec2602
d-w-moore <dmoore@renci.org>
2024-03-01 05:54:35 -0500

    tighter checking in driver/test-runner

--- irods/test/harness/docker_container_driver.sh
+++ irods/test/harness/docker_container_driver.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+#TODO : RUN_AS_USER (default "") and KILL_TEST_CONTAINER (default 1) should be options
+
+: ${KILL_TEST_CONTAINER:=1}
+RUN_AS_USER=""
+
+if [ "$1" = -u ]; then
+    RUN_AS_USER="$2"
+    shift 2
+fi
+
+if [ "$1" = "" ]; then
+    echo >&2 "Usage: $0 /path/to/script"
+    exit 1
+fi
+
 testscript=${1}
 testscript_abspath=$(realpath "$testscript")
 DIR=$(dirname $0)
@@ -9,8 +24,8 @@ declare -A images=(
     [test_1.sh]=install-irods
     [test_2.sh]=bats-python3
     [test_3.bats]=bats-python3
-
     [experiment.sh]=ssl-and-pam
+    [fail.sh]=ssl-and-pam
 )
 image=${images[$(basename $testscript)]}
 reporoot=$(realpath ./tests/repo)
@@ -25,16 +40,17 @@ TIME0=$(date +%s)
 while :; do
     [ `date +%s` -gt $((TIME0 + 30)) ] && { echo >&2 "Waited too long for DB and iRODS to start"; exit 124; }
     sleep 1 
-    docker exec $C grep '(0)' /tmp/irods_status 2>/dev/null
+    docker exec $C grep '(0)' /tmp/irods_status 2>/dev/null >/dev/null
     [ $? -ne 0 ] && { echo -n . >&2; continue; }
     break
 done
 
-docker exec $C $INNER_MOUNT/$(realpath --relative-to $reporoot "$testscript_abspath")
+docker exec ${RUN_AS_USER:+"-u$RUN_AS_USER"} $C $INNER_MOUNT/$(realpath --relative-to $reporoot "$testscript_abspath")
 STATUS=$?
 
-# If undefined, empty or zero-valued, this environment variable allows leaking.
-if [ $((0+LEAK_TEST_CONTAINERS)) -eq 0 ]; then
+# If KILL_TEST_CONTAINER is undefined, empty or zero-valued, don't kill container after script run, ie "leak" the container.
+if [ $((0+KILL_TEST_CONTAINER)) -ne 0 ]; then
     echo >&2 'Killed:' $(docker stop --signal=KILL $C)
 fi
+
 exit $STATUS
