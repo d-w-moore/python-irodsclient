16a45049f86bad163ad18c9bdb22d7c1bb63d694
d-w-moore <dmoore@renci.org>
2024-03-18 05:21:32 -0400

    extra args to start_container go to docker-compose build

--- docker-testing/start_containers.sh
+++ docker-testing/start_containers.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-[ -n "$1" ] || { echo >&2 "requires python_version as argument"; exit 1; }
+PYTHON_VERSION=$1
+shift
+[ -n "$PYTHON_VERSION" ] || { echo >&2 "requires python_version as argument"; exit 1; }
 set -e
 DIR=$(dirname "$0")
 cd "${DIR}"
-echo "python_version=${1}" >.env
+echo "python_version=${PYTHON_VERSION}" >.env
 echo "repo_external=$(./print_repo_root_location)" >>.env
 
-docker-compose -f harness-docker-compose.yml build
+docker-compose -f harness-docker-compose.yml build $*
 docker-compose -f harness-docker-compose.yml up -d
