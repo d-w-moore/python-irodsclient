f5da6799a0ffddd9f198b6844fcf504de473e0d5
d-w-moore <dmoore@renci.org>
2024-03-19 09:42:27 -0400

    succesfully iinit/run as user

--- docker-testing/run_tests.sh
+++ docker-testing/run_tests.sh
@@ -9,21 +9,22 @@ cd "$DIR"
 $PYTHON ./recv_oneshot -h irods-catalog-provider -p 8888 -t 360
 REPO="$(./print_repo_root_location)"
 $PYTHON -m pip install "$REPO[tests]"
-$PYTHON ./iinit.py \
+
+if [ -d /irods_shared ]; then
+    groupadd -o -g $(stat -c%g /irods_shared) irods      # Appropriate the integer codes for irods group ...
+    useradd -g irods -u $(stat -c%u /irods_shared) irods # ... and user.
+    mkdir /irods_shared/{tmp,reg_resc}
+    chown irods.irods /irods_shared/{tmp,reg_resc}
+    chmod 777 /irods_shared/reg_resc
+    chmod g+ws /irods_shared/tmp
+    useradd -G irods -m -s/bin/bash user
+fi
+
+su - user -c "\
+$PYTHON '$DIR'/iinit.py \
     host irods-catalog-provider \
     port 1247 \
     user rods \
     password rods \
     zone tempZone
-
-if [ -d /irods_shared ]; then
-    groupadd -o -g $(stat -c%g /irods_shared) irods  # Appropriate the integer codes for irods group ...
-    useradd $(stat -c%u /irods_shared) irods         # ... and user.
-    mkdir /irods_shared/{tmp,reg_resc}
-    chown irods.irods /irods_shared/{tmp,reg_resc}
-    chmod 777 /irods_shared/reg_resc
-    chmod g+ws /irods_shared/tmp
-    useradd -o -G irods -m -s/bin/bash user
-fi
-
-su - user -c "$PYTHON '$REPO'/irods/test/runner.py"
+$PYTHON '$REPO'/irods/test/runner.py"
--- docker-testing/start_containers.sh
+++ docker-testing/start_containers.sh
@@ -7,6 +7,7 @@ DIR=$(dirname "$0")
 cd "${DIR}"
 echo "python_version=${PYTHON_VERSION}" >.env
 echo "repo_external=$(./print_repo_root_location)" >>.env
+echo "parent_pid=$$" >>.env
 
 docker compose -f harness-docker-compose.yml build $*
 docker compose -f harness-docker-compose.yml up -d
