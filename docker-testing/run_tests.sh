bd627eba5940553b0ffc29284278b04a6b614944
d-w-moore <dmoore@renci.org>
2024-03-18 22:22:12 -0400

    python2/3 agnostic

--- docker-testing/run_tests.sh
+++ docker-testing/run_tests.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 set -e -x
+PYTHON=$(which python3)
+if [ -z "$PYTHON" ]; then
+    PYTHON=$(which python)
+fi
 DIR=$(dirname "$0")
 cd "$DIR"
-./recv_oneshot -h irods-catalog-provider -p 8888 -t 360
+$PYTHON ./recv_oneshot -h irods-catalog-provider -p 8888 -t 360
 REPO="$(./print_repo_root_location)"
-python -m pip install "$REPO[tests]"
-./iinit.py \
+$PYTHON -m pip install "$REPO[tests]"
+$PYTHON ./iinit.py \
     host irods-catalog-provider \
     port 1247 \
     user rods \
