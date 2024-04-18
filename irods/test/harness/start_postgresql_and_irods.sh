8ba063c22f11ab93609cf3d468f56bd5a7d8d0d6
d-w-moore <dmoore@renci.org>
2024-04-18 02:24:29 -0400

    $SUDO -> sudo

--- irods/test/harness/start_postgresql_and_irods.sh
+++ irods/test/harness/start_postgresql_and_irods.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 service postgresql start
 x=${DB_WAIT_SEC:-20}
-while [ $x -ge 0 ] && { ! $SUDO su - postgres -c "psql -c '\l' >/dev/null 2>&1" || x=""; }
+while [ $x -ge 0 ] && { ! sudo su - postgres -c "psql -c '\l' >/dev/null 2>&1" || x=""; }
 do
   [ -z "$x" ] && break
   echo >&2 "$((x--)) secs til database timeout"; sleep 1
