d327e0b143767827bfe37323923f4b213e376d35
d-w-moore <dmoore@renci.org>
2025-02-22 21:25:22 -0500

    patch for consistency in starting

--- irods/test/harness/start_postgresql_and_irods.sh
+++ irods/test/harness/start_postgresql_and_irods.sh
@@ -13,8 +13,16 @@ if ! id -u irods >/dev/null 2>&1 ; then
     IRODS_VSN=$(jq -r '.irods_version' $VERSION_file) /install.sh 5
 fi
 su - irods -c '~/irodsctl restart'
-pgrep irodsServer
-STATUS=$?
+IRODS_WAIT_SEC=20
+x=$IRODS_WAIT_SEC
+SLEEPTIME=""
+while [ $((x--)) -gt 0 ]; do
+  sleep $((SLEEPTIME+0))
+  pgrep irodsServer
+  STATUS=$?
+  [ $STATUS -eq 0 ] && break
+  SLEEPTIME=1
+done
 echo "($STATUS)" >/tmp/irods_status
 [ $STATUS -eq 0 ] || exit 125
 tail -f /dev/null
