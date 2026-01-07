1b42f976391f529dcb7a1b043cc0849d94687de2
d-w-moore <dmoore@renci.org>
2026-01-07 11:46:12 -0500

    test condition wait is ten minutes is the default, no need to specify in call

--- irods/test/modules/test_signal_handling_in_multithread_get.py
+++ irods/test/modules/test_signal_handling_in_multithread_get.py
@@ -19,7 +19,17 @@ _clock_polling_interval = max(0.01, time.clock_getres(time.CLOCK_BOOTTIME))
 
 LARGE_TEST_TIMEOUT = (10 * 60.0) # ten minutes.
 
-def wait_till_true(function, timeout=LARGE_TEST_TIMEOUT):
+def wait_till_true(function, timeout=LARGE_TEST_TIMEOUT, msg = ''):
+    """Wait for test purposes until a condition becomes true , as determined by the
+    return value of the provided test function.
+
+    By default, we wait at most LARGE_TEST_TIMEOUT seconds for the function to return true, and then
+    quit or time out.  Alternatively, a timeout of None translates as a request never to time out.
+
+    If the msg value passed in is a nonzero-length string, it can be used to raise a timeout exception;
+    otherwise timing out causes a normal exit, relaying as the return value the last value returned
+    from the test function.
+    """
     start_time = time.clock_gettime_ns(time.CLOCK_BOOTTIME)
     while not (truth_value := function()):
         if (
@@ -27,7 +37,10 @@ def wait_till_true(function, timeout=LARGE_TEST_TIMEOUT):
             and (time.clock_gettime_ns(time.CLOCK_BOOTTIME) - start_time) * 1e-9
             > timeout
         ):
-            break
+            if msg:
+                raise TimeoutError(msg)
+            else:
+                break
         time.sleep(_clock_polling_interval)
     return truth_value
 
@@ -56,12 +69,10 @@ def test(test_case, signal_names=("SIGTERM", "SIGINT")):
             # unless measures are taken (#722).
             localfile = process.stdout.readline().strip()
             # Use timeout of 10 minutes for test transfer, which should be more than enough.
-            GENEROUS_TIMEOUT = 10*60
             test_case.assertTrue(
                 wait_till_true(
                     lambda: os.path.exists(localfile)
                     and os.stat(localfile).st_size > OBJECT_SIZE // 2,
-                    timeout=GENEROUS_TIMEOUT
                 ),
                 "Parallel download from data_objects.get() probably experienced a fatal error before spawning auxiliary data transfer threads.",
             )
